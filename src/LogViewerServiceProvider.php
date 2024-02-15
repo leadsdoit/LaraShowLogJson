953d2c98c61dd2e8b903fcd4783f3b24ca40b98a
susdcjudgement <sus.a@dcjudgement.com>
2024-02-16 01:35:23 +0300

    vfoes

--- src/LogViewerServiceProvider.php
+++ src/LogViewerServiceProvider.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Ldi\LogViewer;
 
-use src\Providers\PackageServiceProvider;
+use Ldi\Support\Providers\PackageServiceProvider;
 
 class LogViewerServiceProvider extends PackageServiceProvider
 {
