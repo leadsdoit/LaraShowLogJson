ea970f39ad32a09fd7280b2318671d7549472d4a
susdcjudgement <sus.a@dcjudgement.com>
2024-02-16 01:53:19 +0300

    vfoes

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Ldi\LogViewer\Providers;
 
 use Ldi\LogViewer\Http\Routes\LogViewerRoute;
-use src\Providers\RouteServiceProvider as ServiceProvider;
+use Ldi\Support\Providers\RouteServiceProvider as ServiceProvider;
 
 /**
  * Class     RouteServiceProvider
